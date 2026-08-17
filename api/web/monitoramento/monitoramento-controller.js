c9b588b3fcb95f599f7dc5eb7d01ffa90d941518
Jardeon J M Araujo <70022376+JardeonJMAraujo@users.noreply.github.com>
2026-08-17 20:48:28 -0300

    Funcionalidade - Melhoria de filtros no módulo de Monitoramento

--- api/web/monitoramento/monitoramento-controller.js
+++ api/web/monitoramento/monitoramento-controller.js
@@ -8,6 +8,12 @@ router.route('/:id').get(service.buscar);
 router.route('/').post(service.inserir);
 router.route('/:id').patch(service.atualizar);
 router.route('/:id').delete(service.remover);
+router.route('/comboUePorIdContrato').post(service.comboUePorIdContrato);
+router.route('/comboPrestadorServicoPorIdContrato').post(service.comboPrestadorServicoPorIdContrato);
+router.route('/comboContratoPorIdPrestadorServico').post(service.comboContratoPorIdPrestadorServico);
+router.route('/comboUePorIdPrestadorServico').post(service.comboUePorIdPrestadorServico);
+router.route('/comboContratoPorIdUe').post(service.comboContratoPorIdUe);
+router.route('/comboPrestadorServicoPorIdUe').post(service.comboPrestadorServicoPorIdUe);
 
 router.route('/verificaSeDataEferiado').post(service.verificaSeDataEferiado);
 
--- api/web/monitoramento/monitoramento-service.js
+++ api/web/monitoramento/monitoramento-service.js
@@ -19,6 +19,13 @@ exports.atualizar = atualizar;
 exports.remover = remover;
 exports.verificaSeDataEferiado = verificaSeDataEferiado;
 
+exports.comboUePorIdContrato = comboUePorIdContrato;
+exports.comboPrestadorServicoPorIdContrato = comboPrestadorServicoPorIdContrato;
+exports.comboContratoPorIdPrestadorServico = comboContratoPorIdPrestadorServico;
+exports.comboUePorIdPrestadorServico = comboUePorIdPrestadorServico;
+exports.comboContratoPorIdUe = comboContratoPorIdUe;
+exports.comboPrestadorServicoPorIdUe = comboPrestadorServicoPorIdUe;
+
 
 async function buscar(req, res) {
 
@@ -278,4 +285,28 @@ async function verificaSeDataEferiado(req, res) {
   }
   const feriado = await dao.buscaFeriadoUEPorData(req.body.data, req.body.idUnidadeEscolar);
   return await ctrl.gerarRetornoOk(res, feriado );
+}
+
+async function comboUePorIdContrato(req, res) {
+  return await ctrl.gerarRetornoOk(res, await dao.comboUePorIdContrato(req.body.idContrato));
+}
+
+async function comboPrestadorServicoPorIdContrato(req, res) {
+  return await ctrl.gerarRetornoOk(res, await dao.comboPrestadorServicoPorIdContrato(req.body.idContrato));
+}
+
+async function comboContratoPorIdPrestadorServico(req, res) {
+  return await ctrl.gerarRetornoOk(res, await dao.comboContratoPorIdPrestadorServico(req.body.idPrestadorServico));
+} 
+
+async function comboUePorIdPrestadorServico(req, res) {
+  return await ctrl.gerarRetornoOk(res, await dao.comboUePorIdPrestadorServico(req.body.idPrestadorServico));
+} 
+
+async function comboContratoPorIdUe(req, res) {
+  return await ctrl.gerarRetornoOk(res, await dao.comboContratoPorIdUe(req.body.idUe));
+} 
+
+async function comboPrestadorServicoPorIdUe(req, res) {
+  return await ctrl.gerarRetornoOk(res, await dao.comboPrestadorServicoPorIdUe(req.body.idUe));
 }
